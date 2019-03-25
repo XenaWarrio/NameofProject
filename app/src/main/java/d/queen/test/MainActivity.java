bd02c4b91eb62e53f66c67afc069a2ba7285fe97
XenaWarrio <queen.de.xena@gmail.com>
2019-03-25 09:42:56 +0200

    Adding TOAST

--- app/src/main/java/d/queen/test/MainActivity.java
+++ app/src/main/java/d/queen/test/MainActivity.java
@@ -1,10 +1,12 @@
 package d.queen.test;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +27,12 @@ public void OnButtonClick (View v){
     int res = num1 + num2;
 
     result.setText(Integer.toString(res));
+
+    Toast toast =  Toast.makeText(getApplicationContext(),
+                "Аккуратно, тут присутсвует высшая математика",
+            Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
+            toast.show();
 }
 
 }
